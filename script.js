f0012bb412ba94fa03beef9add8dd7069f43257a
Huu_Thinh <huuthinh494@gmail.com>
2023-04-10 22:31:41 +0700

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.getElementById("menu").addEventListener("click", function() {
+  window.location.href = "https://kfcvietnam.com.vn/menu";
+});
+document.getElementById("promotion").addEventListener("click", function() {
+  window.location.href = "https://kfcvietnam.com.vn/menu/hot-deal";
+});
+document.getElementById("stores").addEventListener("click", function() {
+  window.location.href = "https://kfcvietnam.com.vn/he-thong-nha-hang-kfc";
+});
+
+document.getElementById("myButton").addEventListener("click", function() {
+  window.location.href = "https://kfcvietnam.com.vn/store-selection";
+});
+
+document.getElementById("sign_in").addEventListener("click", function() {
+  window.location.href = "https://kfcvietnam.com.vn/account/login?returnUrl=%2F";
+});
